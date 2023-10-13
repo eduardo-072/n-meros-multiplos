4a7fe1756944b3c462b99758e8c4105d5cbca432
eduardo-072 <eduardosilvaaa072@gmail.com>
2023-10-13 17:57:55 -0300

    tarefa concluida

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("---NÚMEROS MULTIPLOS---\n");
+
+Console.Write("DIGITE UM NÚMERO.....:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("VERIFICAR SE O NÚMERO É MULTIPLO DE.....:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 % n2 == 0)
+{
+    Console.WriteLine($"\n{n1} é multiplo de {n2}");
+}
+else
+{
+    Console.WriteLine($"\n{n1} não é multiplo de {n2}");
+
+}
